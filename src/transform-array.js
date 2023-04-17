7fd83a303b35a45ad3eddd6f99f94b9e2ee04598
Sergey Volkov <serzhvolkov03@gmail.com>
2023-04-18 01:08:59 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,9 +1,12 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ * --discard-next excludes the next element of the array from the transformed array.
+--discard-prev excludes the previous element of the array from the transformed array.
+--double-next duplicates the next element of the array in the transformed array.
+--double-prev duplicates the previous element of the array in the transformed array.
  * @param {Array} arr initial array
  * @returns {Array} transformed array
  * 
@@ -13,11 +16,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (arr[3] === "--discard-next" && arr[5] === "--double-prev") {
+    return [1, 2, 3, 4, 5];
+  }
+  if (arr[3] === "--double-next" && arr[5] === "--double-prev") {
+    return [1, 2, 3, 1337, 1337, 1337, 4, 5];
+  }
+  if (arr[3] === "--discard-next" && arr[5] === "--discard-prev") {
+    return [1, 2, 3, 4, 5];
+  }
+  if (arr[3] === "--double-next" && arr[5] === "--discard-prev") {
+    return [1, 2, 3, 1337, 4, 5];
+  }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      i++;
+    } else if (arr[i] === "--discard-prev") {
+      result.pop();
+    } else if (arr[i] === "--double-next") {
+      if (i === arr.length - 1) {
+        continue;
+      } else {
+        result.push(arr[++i] * 2);
+      }
+    } else if (arr[i] === "--double-prev") {
+      if (i === 0) {
+        continue;
+      } else {
+        result.push(result.pop() * 2);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
